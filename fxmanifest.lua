ad213467861d88f307607b068a3aed225f2ca7f8
stickman934 <stickman.z.934@gmail.com>
2024-03-18 15:02:23 +0100

    update fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+author 'stickman934'
+description 'Simple door lock script for FiveM'
+version '1.0.0'
+
 server_scripts {
     'config.lua',
     'server.lua'
